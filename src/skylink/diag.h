6c73f527b04ecce1d73ade23271c4c1cf4707515
petrinm <petrinm@gmail.com>
2021-05-11 10:47:58 +0300

    Better debug printf-defines

--- src/skylink/diag.h
+++ src/skylink/diag.h
@@ -1,7 +1,6 @@
 #ifndef __SKYLINK_DIAG_H__
 #define __SKYLINK_DIAG_H__
 
-#include <stdio.h>
 
 /*
  * Diagnostics and debug tools
@@ -16,24 +15,47 @@
 #define SKY_DIAG_FRAMES     0x0100
 #define SKY_DIAG_BUFFER     0x0200
 
+
 #ifdef DEBUG
-#define SKY_ASSERT(x)    if (x) while(1);
+
+#ifdef __unix__
+#include <assert.h>
+/* Assert for POSIX platforms */
+#define SKY_ASSERT(...)   assert(__VA_ARGS__);
 #else
+/* Assert for embedded platforms */
+#define SKY_ASSERT(x)    if (x) while(1);
+#endif
+
+#else
+/* No asserts  in release build */
 #define SKY_ASSERT(...)   do { } while(0)
 #endif
 
+
+/* Global define for debug print flags */
 extern unsigned int sky_diag_mask;
 
-// TODO: Better defines
-#ifndef STM32F446xx
+
+#ifdef DEBUG
+
+#ifdef __unix__
+#include <stdio.h>
+/* printf for POSIX platforms */
 #define SKY_PRINTF(x, ...) if ((sky_diag_mask & (x)) != 0) { fprintf(stderr, __VA_ARGS__); }
-#elif 1
+#else
+/* printf for embedded platforms */
 #include "SEGGER_RTT.h"
 #define SKY_PRINTF(x, ...) if ((sky_diag_mask & (x)) != 0) { SEGGER_RTT_printf(0, __VA_ARGS__); }
+#endif
+
 #else
+/* No debug prints in release build */
 #define SKY_PRINTF(...) do { } while(0)
 #endif
 
+
+/**/
 void sky_diag_dump_hex(uint8_t* data, unsigned int len);
 
 #endif /* __SKYLINK_DIAG_H__ */
