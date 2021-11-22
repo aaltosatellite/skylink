47711c3a9bc487a765699e47fdd4e47ea9ad6bee
petrinm <petrinm@gmail.com>
2021-11-22 17:29:28 +0200

    Sync the configuration changes to gs

--- gs/main.c
+++ gs/main.c
@@ -72,17 +72,11 @@ int main(int argc, char *argv[])
 	config->identity[3] = 'S';
 	config->identity[4] = '1';
 
-	config->vc_priority[0] = 0;
-	config->vc_priority[1] = 1;
-	config->vc_priority[2] = 2;
-	config->vc_priority[3] = 3;
-
 	/*
 	 * MAC configurations
 	 */
-	config->mac.maximum_gap_length           = 1000; // [ms]
-	config->mac.minimum_gap_length           = 50;  // [ms]
-	config->mac.default_gap_length           = 600; // [ms]
+	config->mac.default_gap_length           = 700; // [ms]
+	config->mac.default_tail_length          = 86;  // [ms]
 
 	config->mac.maximum_window_length        = 350; // [ms]
 	config->mac.minimum_window_length        = 25;  // [ms]
@@ -90,54 +84,39 @@ int main(int argc, char *argv[])
 
 	config->mac.default_tail_length          = 86;  // [ms]
 	config->mac.unauthenticated_mac_updates  = 0;   // [ms]
+	config->mac.shift_threshold_ms           = 4000; // [ms]
 
 	/*
 	 * ARQ configurations
 	 */
-	config->vc[0].arq_on = 1;
-	config->vc[0].require_authentication     = 0;
-	config->vc[1].arq_on = 1;
+	config->vc[0].require_authentication     = 1;
 	config->vc[1].require_authentication     = 0;
-	config->vc[2].arq_on = 0;
 	config->vc[2].require_authentication     = 0;
-	config->vc[3].arq_on = 0;
 	config->vc[3].require_authentication     = 0;
 
-	config->array[0].n_recall                = 16;
 	config->array[0].horizon_width           = 16;
 	config->array[0].send_ring_len           = 24;
 	config->array[0].rcv_ring_len            = 24;
 	config->array[0].element_count           = 3600;
 	config->array[0].element_size            = 36;
-	config->array[0].initial_send_sequence   = 0;
-	config->array[0].initial_rcv_sequence    = 0;
 
-	config->array[1].n_recall                = 16;
 	config->array[1].horizon_width           = 16;
 	config->array[1].send_ring_len           = 24;
 	config->array[1].rcv_ring_len            = 24;
 	config->array[1].element_count           = 3600;
 	config->array[1].element_size            = 36;
-	config->array[1].initial_send_sequence   = 0;
-	config->array[1].initial_rcv_sequence    = 0;
 
-	config->array[2].n_recall                = 0;
 	config->array[2].horizon_width           = 0;
 	config->array[2].send_ring_len           = 8;
 	config->array[2].rcv_ring_len            = 8;
 	config->array[2].element_count           = 800;
 	config->array[2].element_size            = 36;
-	config->array[2].initial_send_sequence   = 0;
-	config->array[2].initial_rcv_sequence    = 0;
 
-	config->array[3].n_recall                = 0;
 	config->array[3].horizon_width           = 0;
 	config->array[3].send_ring_len           = 8;
 	config->array[3].rcv_ring_len            = 8;
 	config->array[3].element_count           = 800;
 	config->array[3].element_size            = 36;
-	config->array[3].initial_send_sequence   = 0;
-	config->array[3].initial_rcv_sequence    = 0;
 
 	/*
 	 * HMAC configuration
@@ -214,5 +193,5 @@ int main(int argc, char *argv[])
 
 	}
 
-	return 0;
+
 }
