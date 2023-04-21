9c2dec80f67f864ea5ff2e223cff34ff48513ae6
Felipe Tampier <felipetampier@gmail.com>
2023-04-21 12:08:40 +0300

    Added sky_config struct for debugging

--- scripts/skylink_learning_first_steps.py
+++ scripts/skylink_learning_first_steps.py
@@ -30,8 +30,75 @@ TEMPLATE_RESPONSE_FRAME = {
     }
 }
 
+# Manually parsing packets as received by suo terminal 
 import skylink
 s = "66 4f 48 32 41 53 33 2a 05 04 23 54 00 fa 00 f7 ab 21 62 52 23 6d d6 be a0 b2"     # Skymodem received packet
 print(skylink.parse(bytes.fromhex(s)))
 
+# Parse SkylinkConfig
+class SkyConfig(NamedTuple):
+    # SkyPHYConfig:
+    enable_scrambler: int
+    enable_rs: int
 
+    # SkyMACConfig:
+    maximum_window_length_ticks: int
+    minimum_window_length_ticks: int
+    gap_constant_ticks: int
+    tail_constant_ticks: int
+    shift_threshold_ticks: int
+    idle_timeout_ticks: int
+    window_adjust_increment_ticks: int
+    carrier_sense_ticks: int
+    unauthenticated_mac_updates: int
+    window_adjustment_period: int
+    idle_frames_per_window: int
+
+    # SkyHMACConfig:
+    hmac_key_length: int
+    hmac_maximum_jump: int
+    hmac_key: bytes
+
+    # SkyVCConfig:
+    vc0_element_size: int
+    vc0_rcv_ring_len: int
+    vc0_horizon_width: int
+    vc0_send_ring_len: int
+    vc0_require_authentication: int
+
+    # SkyVCConfig:
+    vc1_element_size: int
+    vc1_rcv_ring_len: int
+    vc1_horizon_width: int
+    vc1_send_ring_len: int
+    vc1_require_authentication: int
+
+    # SkyVCConfig:
+    vc2_element_size: int
+    vc2_rcv_ring_len: int
+    vc2_horizon_width: int
+    vc2_send_ring_len: int
+    vc2_require_authentication: int
+
+    # SkyVCConfig:
+    vc3_element_size: int
+    vc3_rcv_ring_len: int
+    vc3_horizon_width: int
+    vc3_send_ring_len: int
+    vc3_require_authentication: int
+
+    # Identity
+    identity: bytes
+
+    # SkyARQConfig:
+    arq_timeout_ticks: int
+    arq_idle_frame_threshold: int
+    arq_idle_frames_per_window: int
+
+data = lambda x: SkyConfig(*struct.unpack("<2Bxx" +    # PHY config
+                                   "6i2hBbBx" + # TDD MAC config
+                                   "ii32s" +    # HMAC config
+                                   4*"4iBxxx" + # Virtual channel config
+                                   "6sxx" +     # ID
+                                   "2iBxxx",    # ARQ config
+                                   x))
