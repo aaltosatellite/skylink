c94a2074d26ffc44a713eb4841c2226107a449fc
Markus <m.v.hiltunen@gmail.com>
2025-04-14 07:58:40 +0300

    ...

--- skymodem/radio_loop.py
+++ skymodem/radio_loop.py
@@ -272,7 +272,7 @@ class RadioLoop:
 		rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
 		timeout = int(1e6 * bufferlen * 0.8 / self.radio_config.rx_sr0)
 		n_rx_loops = 0
-		buff = np.zeros(bufferlen*1024, np.complex64)
+		buff = np.zeros(bufferlen*600, np.complex64)
 		absolute_bufflen = len(buff)
 		n_rx_total = 0
 		avg_sr = 0.0
