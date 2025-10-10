24c28543f8b7a803cbd9397a1d22315da11e38fb
Markus <m.v.hiltunen@gmail.com>
2025-10-10 17:17:43 +0300

    burst test added to radioloop_tests.py

--- skymodem/dsp_library/radioloop_tests.py
+++ skymodem/dsp_library/radioloop_tests.py
@@ -15,12 +15,13 @@ def blind_sink(que:Queue):
 		except Empty:
 			continue
 
-def make_powertest_samples(sr, t_array, bw=0.1):
-	assert 0.0 < bw < 1.0
+def make_powertest_samples(sr, t_array, offset=0.1):
+	assert abs(offset) < 0.5
 	#lptaps = firwin(121, bw, pass_zero=True)
 	nsamples = int(sr*t_array)
-	arr = np.exp(2j*np.pi * (np.arange(nsamples)*0.1) )
+	arr = np.exp(2j*np.pi * (np.arange(nsamples)*offset) )
 	return arr
+
 
 
 
@@ -37,14 +38,6 @@ def t1_connect_and_observe():
 	print("Closing.")
 	radioloop.close()
 	print("Closed")
-	#time.sleep(1.0)
-	#que_rx_samples_out.put("QUIT")
-
-
-
-
-
-
 
 
 
@@ -60,10 +53,9 @@ def t2_continuous_transmit(mode, tx_gain):
 	radioloop = RadioLoop(radio_config=config, que_tx_samples_in=que_tx_samples_in, que_rx_samples_out=que_rx_samples_out)
 	radioloop.tx_gain0 = tx_gain
 	radioloop.start()
-	print("Waiting for radio to start (15s)")
-	time.sleep(15.0)
-
-	tx_samples = make_powertest_samples(sr=sr00, t_array=0.5, bw=0.1)
+	print("Waiting for radio to start (6s)")
+	time.sleep(6.0)
+	tx_samples = make_powertest_samples(sr=sr00, t_array=0.5, offset=0.1)
 	tx_samples = np.complex64(tx_samples)
 	tx_samples = np.reshape(tx_samples, (1,len(tx_samples)))
 	i = 0
@@ -74,19 +66,38 @@ def t2_continuous_transmit(mode, tx_gain):
 		i = (i+1) % 10
 
 
-	#time.sleep(40)
-	#print("Closing.")
-	#radioloop.close()
-	#print("Closed")
-	#time.sleep(1.0)
-	#que_rx_samples_out.put("QUIT")
+
+def t3_single_burst(mode, tx_gain, burst_duration):
+	assert burst_duration < 15.0, "Too long burst for a single samplearray"
+	assert mode in ("usrp", "soapy")
+	sr00 = 2e6
+	f00 = 437.000e6
+	config = RadioConfig(mode=mode, rx_sr=sr00, rx_f_tune=f00, tx_sr=sr00, tx_f_tune=f00)
+	que_tx_samples_in = Queue(2)
+	que_rx_samples_out = Queue(100)
+	sinkt = threading.Thread(target=blind_sink, args=(que_rx_samples_out,), daemon=True)
+	sinkt.start()
+	radioloop = RadioLoop(radio_config=config, que_tx_samples_in=que_tx_samples_in, que_rx_samples_out=que_rx_samples_out)
+	radioloop.tx_gain0 = tx_gain
+	radioloop.start()
+	print("Waiting for radio to start (6s)")
+	time.sleep(6.0)
+	tx_samples = make_powertest_samples(sr=sr00, t_array=burst_duration, offset=0.1)
+	tx_samples = np.complex64(tx_samples)
+	tx_samples = np.reshape(tx_samples, (1,len(tx_samples)))
+	que_tx_samples_in.put(tx_samples.copy(), timeout=2.0)
+	time.sleep(burst_duration+0.2)
+	print("Closing.")
+	radioloop.close()
+	print("Closed")
 
 
 
 
-t2_continuous_transmit(mode="usrp", tx_gain=4)
+
 #t1_connect_and_observe()
-
+t2_continuous_transmit(mode="usrp", tx_gain=4)
+t3_single_burst(mode="usrp", tx_gain=0, burst_duration=2.0)
 
 
 
