a135aaf285338d2a2763a8d3b2a868c2e29f3d35
Markus <m.v.hiltunen@gmail.com>
2025-11-13 16:14:58 +0200

    multimode fix 2

--- skymodem/dsp_library/kuokka/dsp_loop.py
+++ skymodem/dsp_library/kuokka/dsp_loop.py
@@ -331,7 +331,7 @@ class DSPLoop:
 			with self.rlock:
 				while not que_mpr_processes_out.empty():
 					rcode, p_idd, tup = que_mpr_processes_out.get_nowait()
-					if (rcode == "cs") and (not self._tx_on(t_mono=tup[0])) and ((ts_mono - ts_last_cs) > 20e-3):
+					if (rcode == "cs") and (not self._tx_on(t_mono=tup[0])) and ((tup[0] - ts_last_cs) > 20e-3):
 						ts_mono, ts_unix = tup[:2]
 						self.que_rx_payloads_out.put(("cs", None, ts_mono), timeout=1.0)
 						ts_last_cs = ts_mono
